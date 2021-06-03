2aeeb07428b58dc35db550bdb790e97a5f232176
Samyak Jain <samyakjain971@gmail.com>
2021-06-03 14:17:31 +0530

    problemHai

--- src/App.js
+++ src/App.js
@@ -1,6 +1,71 @@
 import "./App.css";
+import React from 'react';
 const crypto = require("crypto"); // crypto comes with Node.js
 
+// https://github.com/zoom/zoom-api-jwt
+const jwt = require('jsonwebtoken');
+const rp = require('request-promise');
+var apiKey = "WkLcrPpYTD2LAI9MN5fybw";
+var apiSecret = "pYqRltSNafNnGaLc4ZbN20ffSe66u3BZ6iQu";
+var email = "samyak.21810494@viit.ac.in" //doctor's Email
+//Use the ApiKey and APISecret from above
+const payload = {
+    iss: apiKey,
+    exp: ((new Date()).getTime() + 5000)
+};
+const token = jwt.sign(payload, apiSecret);
+
+var options = {
+    //You can use a different uri if you're making an API call to a different Zoom endpoint.
+    uri: "https://api.zoom.us/v2/users/"+email, 
+    qs: {
+        status: 'active' 
+    },
+    auth: {
+        'bearer': token
+    },
+    headers: {
+        'User-Agent': 'Zoom-api-Jwt-Request',
+        'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+};
+
+// function meetingGenerate(options) {
+// 	//printing the response on the console
+// 	try{
+// 		console.log('User has',options); //console.log(typeof response);
+  
+// 		resp = rp.; //Adding html to the page
+// 		res.send(JSON.stringify(resp, null, 2) );
+//   	} catch (err) {
+// 		// API call failed...
+// 		console.log('API call failed, reason ', err);
+//   	};
+// }
+
+function meetingGenerate(options){
+
+ 	var response = rp(options)
+	return JSON.stringify(response, null, 2);
+ 
+    // try{
+    //   //printing the response on the console
+    //     console.log('User has', response);
+    //     //console.log(typeof response);
+    //     var resp = response
+    //     //Adding html to the page
+    //     //Prettify the JSON format using pre tag and JSON.stringify
+    //     // var result = title + '<code><pre style="background-color:#aef8f9;">'+JSON.stringify(resp, null, 2)+ '</pre></code>'
+    //     // res.send(result1 + '<br>' + result);
+	// 	return JSON.stringify(resp, null, 2);
+ 
+    // }
+    // catch (err) {
+    //     // API call failed...
+    //     console.log('API call failed, reason ', err);
+    // };
+}
 function generateSignature(apiKey, apiSecret, meetingNumber, role) {
 	// Prevent time sync issue between client signature generation and zoom
 	var signature;
@@ -25,42 +90,22 @@ ZoomMtg.prepareJssdk();
 
 function App() {
 	// setup your signature endpoint here: https://github.com/zoom/websdk-sample-signature-node.js
-
-	var apiKey = "dkTOd-IzS2emVPW89C7qUg";
-	var meetingNumber = 93649830507;
-	var role = 0;
+	
+	var meetingDetails = meetingGenerate(options);
+	var meetingNumber = meetingDetails.pmi;
+	var role = meetingDetails.role_name;
 	var leaveUrl = "http://localhost:3000";
-	var userName = "RUCHIKA";
-	var userEmail = "bhaisareruchika@gmail.com";
-	var passWord = "c1hFCu";
+	var userName = meetingDetails.first_name + meetingDetails.last_name ;
+	var userEmail = "samyak.21810494@viit.ac.in";
+	var passWord = "zre6aX";
 	var signatureEndpoint = "";
 
 	console.log(signatureEndpoint);
 
-	// function getSignature(e) {
-	// 	e.preventDefault();
-
-	// 	fetch(signatureEndpoint, {
-	// 		method: "POST",
-	// 		headers: { "Content-Type": "application/json" },
-	// 		body: JSON.stringify({
-	// 			meetingNumber: meetingNumber,
-	// 			role: role,
-	// 		}),
-	// 	})
-	// 		.then((res) => res.json())
-	// 		.then((response) => {
-	// 			startMeeting(response.signature);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.error(error);
-	// 		});
-	// }
-
 	function startMeeting() {
 		document.getElementById("zmmtg-root").style.display = "block";
 
-		signatureEndpoint = generateSignature(apiKey, "FaJdZc5L6ILEZgKx6mylKCui2KDHo6vtMAaa", meetingNumber, role);
+		signatureEndpoint = generateSignature(apiKey, apiSecret, meetingDetails.pmi, role);
 
 		ZoomMtg.init({
 			leaveUrl: leaveUrl,
