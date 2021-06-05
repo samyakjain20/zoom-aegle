6bc6dbf0c57522656ef96b5f0df536058d7ea4dc
Samyak Jain <samyakjain971@gmail.com>
2021-06-06 00:52:52 +0530

    base-backend

---
+++ app.js
@@ -0,0 +1,71 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const cors = require('cors');
+const nodemailer = require('nodemailer')
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.post('/contact', (req,res) =>{
+  console.log(req.body)
+  const tranporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'samyak.21810494@viit.ac.in',
+        pass: ""
+    }
+  })
+
+  const mailOptions = {
+      from: req.body.email,
+      to: 'samyak.21810494@viit.ac.in',
+      subject: `Message from ${req.body.name}: ${req.body.subject}`,
+      text: req.body.message
+  }
+
+  tranporter.sendMail(mailOptions, (error, info) => {
+      if(error){
+          console.log(error);
+          res.send('error')
+      }else{
+          console.log('Email Sent: ' + info.response)
+          res.send('success')
+      }
+  })
+
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
